29c8d73bd8162709e6f8331dbbe63afadf2510d2
A.pavan kumar <70189509+pavan1126@users.noreply.github.com>
2020-11-03 10:58:42 +0530

    Add files via upload

---
+++ L10 program on infix to postfix.py
@@ -0,0 +1,36 @@
+#A.pavan kumar - 121910313047
+#progra on infix to postfix
+def isgreater(e1,e2):
+def isgreater(e1,e2):
+	val = {'+': 1,
+		'-': 1,
+		'*': 2,
+		'/': 2}
+	if val[e1]>=val[e2]: return True
+	return False
+
+
+def infixtopostfix(s):
+	char = []
+	res = ""
+	for i in s:
+		if i not in "()*/-+":
+			res+=i
+		elif i in '*/+-':
+			while char!=[] and char[-1]!='(' and isgreater(char[-1],i):
+				res+=char.pop()
+			char.append(i)
+		elif i==')':
+			while char!=[] and char[-1]!='(':
+				res+=char.pop()
+			char.pop()
+		elif i=='(':
+			char.append(i)
+
+	while char:
+		k = char.pop()
+		res+=k
+	print(res)
+
+exp = input("Enter an infix expression : ")
+infixtopostfix(exp)
---
+++ L10 program on postfix to infix.py
@@ -0,0 +1,15 @@
+#A.pavan kumar - 121910313047
+#program on postfix to infix
+def postfix(s):
+	stack = []
+	for i in s:
+		if i not in "*/+-":
+			stack.append(i)
+		else:
+			op1 = stack.pop()
+			op2 = stack.pop()
+			stack.append(op2+i+op1)
+	print(''.join(stack))
+
+expression = input("Enter the postfix expression you want to convert : ")
+postfix(expression)
